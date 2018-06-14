e3661394e9f434fef524b5f53884248a36c878a5
Georgi Georgiev <gogo4ds@gmail.com>
2018-06-15 01:11:44 +0300

    Added seeding of admin

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-const encryption = require('./utils/encryption')
+const encryption = require('../utils/encryption')
+const Role = require('./Role')
 
 let userSchema = mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -23,5 +24,42 @@ const User = mongoose.model('User', userSchema)
 module.exports = User
 
 module.exports.seedAdmin = () => {
+  let username = 'admin'
+  User
+    .findOne({ username: username })
+    .then(user => {
+      if (!user) {
+        Role
+          .findOne({name: 'Admin'})
+          .then(role => {
+            let salt = encryption.generateSalt()
+            let passwordHash = encryption.hashPassword('admin', salt)
 
+            let roles = []
+            roles.push(role.id)
+
+            let user = {
+              username: username,
+              passwordHash: passwordHash,
+              firstName: 'Admin',
+              lastName: 'Adminov',
+              salt: salt,
+              roles: roles
+            }
+
+            User
+              .create(user)
+              .then(user => {
+                role.users.push(user.id)
+                role.save(err => {
+                  if (err) {
+                    console.log(err.message)
+                  } else {
+                    console.log('Admin seeded successfully!')
+                  }
+                })
+              })
+          })
+      }
+    })
 }
